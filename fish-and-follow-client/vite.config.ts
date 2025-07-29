e9302ccdd1452a92702ae352141d5c3d978a96b8
Nate <10155226+nh758@users.noreply.github.com>
2025-07-29 18:55:44 +0900

    set up pwa (#34)
    * set up pwa
    
    * fix inject manifest manually

--- fish-and-follow-client/vite.config.ts
+++ fish-and-follow-client/vite.config.ts
@@ -2,6 +2,8 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from 'vite-plugin-pwa';
+import { manifest } from './manifest';
 
 const devServer = {
   target: process.env.REACT_APP_API_URL ?? 'http://localhost:3000',
@@ -9,7 +11,17 @@ const devServer = {
 };
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate', devOptions: {
+        enabled: true
+      },
+      manifest,
+    }),
+  ],
   server: {
     proxy: {
       '/api': devServer,
